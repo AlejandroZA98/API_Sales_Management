f7a269462fb46f4fbb5dd7ea0ec8b00ff447804d
Alejandro Zacarias <alez42676@gmail.com>
2024-09-13 23:59:31 -0600

    changes in model, serializer and views form payments has been created

--- Sales_Management/sales_management_app/api/models/payments_model.py
+++ Sales_Management/sales_management_app/api/models/payments_model.py
@@ -1,9 +1,10 @@
 from django.db import models
 from django.contrib.auth.models import User
+from sales_management_app.api.models.clients_model import Client
 
 class Payment(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
-    date =models.DateTimeField()
+    client=models.ForeignKey(Client,on_delete=models.CASCADE)  
     amount=models.FloatField()
     merchandise=models.CharField(max_length=150)
     note=models.TextField(blank=False)
--- Sales_Management/sales_management_app/api/serializers/payments_serializer.py
+++ Sales_Management/sales_management_app/api/serializers/payments_serializer.py
@@ -1,11 +1,15 @@
 from rest_framework import serializers
 from sales_management_app.api.models.payments_model import Payment 
 from django.contrib.auth.models import User
+from sales_management_app.api.models.clients_model import Client
 
 class PaymentSerializer(serializers.ModelSerializer):
     user = serializers.PrimaryKeyRelatedField(queryset=User.objects.all(),many=False)  # Permitir el ID del usuario
     user_name = serializers.StringRelatedField(source='user', read_only=True)
     url = serializers.HyperlinkedIdentityField(view_name='payment-detail')
+    client =serializers.PrimaryKeyRelatedField(queryset=Client.objects.all(), many=False)  # Corrección aquí
+
+    client_name = serializers.StringRelatedField(source='client', read_only=True)
     class Meta:
         model=Payment
         fields='__all__'
