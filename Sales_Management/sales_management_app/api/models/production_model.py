8e747e1dadf4e2841ff7523153d23a824501a304
Alejandro Zacarias <alez42676@gmail.com>
2024-09-24 19:09:37 -0600

    productions substract ingredients from the inventary

--- Sales_Management/sales_management_app/api/models/production_model.py
+++ Sales_Management/sales_management_app/api/models/production_model.py
@@ -6,6 +6,7 @@ class Production(models.Model):
     user=models.ForeignKey(User,on_delete=models.CASCADE)
     concept=models.CharField(max_length=120)
     cuantity=models.FloatField()
+    ingredients=models.JSONField()
     created_at=models.DateTimeField(auto_now_add=True)
     updated_at=models.DateTimeField(auto_now=True)
     def __str__(self):
--- Sales_Management/sales_management_app/api/views/create_production_view.py
+++ Sales_Management/sales_management_app/api/views/create_production_view.py
@@ -1,6 +1,7 @@
 
 from sales_management_app.api.models.production_model import Production
 from sales_management_app.api.serializers.production_serializer import ProductionSerializer
+from sales_management_app.api.models.inventary_ingredients_model import InventaryIngredients
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework import status
@@ -8,11 +9,32 @@ from rest_framework import status
 class CreateProductionView(APIView):
     def post(self, request,pk):
         data=request.data
+        ingredients=request.data.get('ingredients')
+        print("INGREDIENTS",ingredients)
         print("DATA",data)
         data['user']=pk
         serializer=ProductionSerializer(data=data,context={'request':request})
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data,status=201)
-        return Response(serializer.errors,status=400)
-    
+      
+        for ingredient in ingredients:
+            print("QUANTITY",ingredient['cuantity'])
+            try:
+                inventory_item = InventaryIngredients.objects.get(ingredient=ingredient['ingredient'])
+                print("inventory",inventory_item)
+
+                if inventory_item.cuantity < ingredient['cuantity']:
+                    return Response(
+                        {"error": f"El ingrediente '{ingredient['ingredient']}' no tiene suficiente stock."}, 
+                        status=400)
+                inventory_item.cuantity -= ingredient['cuantity']*data['cuantity']
+                inventory_item.save()
+                if serializer.is_valid():
+                    serializer.save()
+
+            except InventaryIngredients.DoesNotExist:
+                return Response(
+                {"error": f"El ingrediente '{ingredient['ingredient']}' no se encuentra en el inventario."}, 
+                status=400)
+
+        return Response({
+                'status': 'success',
+                'message': 'Produccion registrada y stock actualizado.',}, status=status.HTTP_201_CREATED)
