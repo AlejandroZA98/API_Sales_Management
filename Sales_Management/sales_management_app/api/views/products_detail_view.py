df05f8628019343b9082add9d10e0d3cc0f0c84e
Alejandro Zacarias <alez42676@gmail.com>
2024-10-03 16:21:19 -0600

    delete product implemented

--- Sales_Management/sales_management_app/api/views/products_detail_view.py
+++ Sales_Management/sales_management_app/api/views/products_detail_view.py
@@ -19,3 +19,11 @@ class ProductDetailView(APIView):
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+    def delete(self, request, pk):
+        try:
+            product = Product.objects.get(pk=pk)
+        except Product.DoesNotExist:
+            return Response({'error': 'Product not found'}, status=status.HTTP_404_NOT_FOUND)
+
+        product.delete()  # Elimina el producto de la base de datos
+        return Response({'message': 'Product deleted successfully'}, status=status.HTTP_204_NO_CONTENT)
